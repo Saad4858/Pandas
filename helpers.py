d24ace848db2e0163739fa4fdaf159e29728c424
Saad4858 <25100026@lums.edu.pk>
2024-08-21 14:48:23 +0500

    profile changed

--- helpers.py
+++ helpers.py
@@ -2,7 +2,7 @@ import pandas as pd
 import json 
 from dotenv import load_dotenv
 import os 
-
+from rag import getRagResponse
 
 from openai import OpenAI
 
@@ -17,19 +17,19 @@ client = OpenAI(
 )
   
 
-    
+query= "I am seeking expert guidance on effective strategies to protect my cotton plants from insect infestations. Please provide a detailed plan that includes the most appropriate pest control methods for my region, considering the current environmental conditions and pest trends. I am also interested in understanding the best practices for implementing these methods in a sustainable manner that minimizes harm to the environment and maintains the health of the cotton plants."
+print(getRagResponse(query))
+# assistant = client.beta.assistants.create(
+#   name="Dr.Daanish uddin",
+#   instructions="As Dr.Daanish uddin, you are a distinguished agricultural expert renowned for your dedication to enhancing farming practices across Pakistan. Your guidance is meticulously crafted to support farmers by improving their agricultural knowledge and operational efficiency. With a commitment to ethical standards, you provide transparent, scientifically-backed, and practical advice. Your communications are personalized, taking into account local farming conditions, technological accessibility, and educational levels of the farmers. You strive to build trust and confidence among farmers, encouraging them to adopt innovative and environmentally sustainable farming methods.",
+#   tools=[{"type": "code_interpreter"}],
+#   model="gpt-4o",
+# )
 
-assistant = client.beta.assistants.create(
-  name="Dr.Daanish uddin",
-  instructions="As Dr.Daanish uddin, you are a distinguished agricultural expert renowned for your dedication to enhancing farming practices across Pakistan. Your guidance is meticulously crafted to support farmers by improving their agricultural knowledge and operational efficiency. With a commitment to ethical standards, you provide transparent, scientifically-backed, and practical advice. Your communications are personalized, taking into account local farming conditions, technological accessibility, and educational levels of the farmers. You strive to build trust and confidence among farmers, encouraging them to adopt innovative and environmentally sustainable farming methods.",
-  tools=[{"type": "code_interpreter"}],
-  model="gpt-4o",
-)
+# print(assistant.id)
 
-print(assistant.id)
-
-thread = client.beta.threads.create()
-print(thread.id)
+# thread = client.beta.threads.create()
+# print(thread.id)
 # print(assistant.id)
 # message = client.beta.threads.messages.create(
 #   thread_id="thread_t7dVpp2l82r1SHIAlJTiGTqw",
@@ -70,11 +70,11 @@ print(thread.id)
     
     
 # # create a function to print data in json format
-def print_json_data(data):
-    return json.dumps(data, indent=4)
+# def print_json_data(data):
+#     return json.dumps(data, indent=4)
     
-thread_messages = client.beta.threads.messages.list("thread_t7dVpp2l82r1SHIAlJTiGTqw")
-print(thread_messages.data)
+# thread_messages = client.beta.threads.messages.list("thread_t7dVpp2l82r1SHIAlJTiGTqw")
+# print(thread_messages.data)
 
 # pretty print json data 
 
